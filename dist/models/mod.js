bb6de1f9b4e19b7b6efd04b0fe5806931eafa47a
RanolP <RanolP@users.noreply.github.com>
2021-09-07 09:09:36 +0000

    Follow-up solvedac/unofficial-documentation

---
+++ dist/models/mod.js
@@ -0,0 +1,141 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * 난이도 아이콘 표시 정책입니다.
+ * @export
+ * @enum {string}
+ */
+export var IconScheme;
+(function (IconScheme) {
+    IconScheme["Default"] = "default";
+    IconScheme["HideWarn"] = "hide-warn";
+    IconScheme["Hide"] = "hide";
+})(IconScheme || (IconScheme = {}));
+/**
+ * 언어입니다.
+ * @export
+ * @enum {string}
+ */
+export var Language;
+(function (Language) {
+    Language["Ko"] = "ko";
+    Language["En"] = "en";
+})(Language || (Language = {}));
+/**
+* @export
+* @enum {string}
+*/
+export var RankedUserClassDecorationEnum;
+(function (RankedUserClassDecorationEnum) {
+    RankedUserClassDecorationEnum["None"] = "none";
+    RankedUserClassDecorationEnum["Silver"] = "silver";
+    RankedUserClassDecorationEnum["Gold"] = "gold";
+})(RankedUserClassDecorationEnum || (RankedUserClassDecorationEnum = {}));
+/**
+* @export
+* @enum {string}
+*/
+export var SchemaClassDecorationEnum;
+(function (SchemaClassDecorationEnum) {
+    SchemaClassDecorationEnum["None"] = "none";
+    SchemaClassDecorationEnum["Silver"] = "silver";
+    SchemaClassDecorationEnum["Gold"] = "gold";
+})(SchemaClassDecorationEnum || (SchemaClassDecorationEnum = {}));
+/**
+* @export
+* @enum {string}
+*/
+export var Schema1ScreenThemeEnum;
+(function (Schema1ScreenThemeEnum) {
+    Schema1ScreenThemeEnum["Default"] = "default";
+    Schema1ScreenThemeEnum["Light"] = "light";
+    Schema1ScreenThemeEnum["Dark"] = "dark";
+    Schema1ScreenThemeEnum["Black"] = "black";
+})(Schema1ScreenThemeEnum || (Schema1ScreenThemeEnum = {})); /**
+* @export
+* @enum {string}
+*/
+export var Schema1ProblemSortByEnum;
+(function (Schema1ProblemSortByEnum) {
+    Schema1ProblemSortByEnum["Id"] = "id";
+    Schema1ProblemSortByEnum["Level"] = "level";
+    Schema1ProblemSortByEnum["Title"] = "title";
+    Schema1ProblemSortByEnum["Solved"] = "solved";
+    Schema1ProblemSortByEnum["AverageTry"] = "average_try";
+    Schema1ProblemSortByEnum["Random"] = "random";
+})(Schema1ProblemSortByEnum || (Schema1ProblemSortByEnum = {}));
+/**
+* @export
+* @enum {string}
+*/
+export var Schema11TypeEnum;
+(function (Schema11TypeEnum) {
+    Schema11TypeEnum["Community"] = "community";
+    Schema11TypeEnum["University"] = "university";
+    Schema11TypeEnum["Company"] = "company";
+    Schema11TypeEnum["HighSchool"] = "high_school";
+})(Schema11TypeEnum || (Schema11TypeEnum = {}));
+/**
+* @export
+* @enum {string}
+*/
+export var Schema6ClassDecorationEnum;
+(function (Schema6ClassDecorationEnum) {
+    Schema6ClassDecorationEnum["None"] = "none";
+    Schema6ClassDecorationEnum["Silver"] = "silver";
+    Schema6ClassDecorationEnum["Gold"] = "gold";
+})(Schema6ClassDecorationEnum || (Schema6ClassDecorationEnum = {}));
+/**
+ * 난이도 아이콘 표시 정책입니다.
+ * @export
+ * @enum {string}
+ */
+export var Schema8;
+(function (Schema8) {
+    Schema8["Default"] = "default";
+    Schema8["HideWarn"] = "hide-warn";
+    Schema8["Hide"] = "hide";
+})(Schema8 || (Schema8 = {}));
+/**
+ * 언어입니다.
+ * @export
+ * @enum {string}
+ */
+export var Schema9;
+(function (Schema9) {
+    Schema9["Ko"] = "ko";
+    Schema9["En"] = "en";
+})(Schema9 || (Schema9 = {}));
+/**
+* @export
+* @enum {string}
+*/
+export var SettingsScreenThemeEnum;
+(function (SettingsScreenThemeEnum) {
+    SettingsScreenThemeEnum["Default"] = "default";
+    SettingsScreenThemeEnum["Light"] = "light";
+    SettingsScreenThemeEnum["Dark"] = "dark";
+    SettingsScreenThemeEnum["Black"] = "black";
+})(SettingsScreenThemeEnum || (SettingsScreenThemeEnum = {})); /**
+* @export
+* @enum {string}
+*/
+export var SettingsProblemSortByEnum;
+(function (SettingsProblemSortByEnum) {
+    SettingsProblemSortByEnum["Id"] = "id";
+    SettingsProblemSortByEnum["Level"] = "level";
+    SettingsProblemSortByEnum["Title"] = "title";
+    SettingsProblemSortByEnum["Solved"] = "solved";
+    SettingsProblemSortByEnum["AverageTry"] = "average_try";
+    SettingsProblemSortByEnum["Random"] = "random";
+})(SettingsProblemSortByEnum || (SettingsProblemSortByEnum = {}));
+/**
+* @export
+* @enum {string}
+*/
+export var UserClassDecorationEnum;
+(function (UserClassDecorationEnum) {
+    UserClassDecorationEnum["None"] = "none";
+    UserClassDecorationEnum["Silver"] = "silver";
+    UserClassDecorationEnum["Gold"] = "gold";
+})(UserClassDecorationEnum || (UserClassDecorationEnum = {}));
