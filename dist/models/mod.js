649593d30867c9e4568c6f41545bf2aec595be63
RanolP <RanolP@users.noreply.github.com>
2021-09-10 07:20:53 +0000

    Follow-up solvedac/unofficial-documentation

--- dist/models/mod.js
+++ dist/models/mod.js
@@ -23,87 +23,23 @@ export var Language;
 * @export
 * @enum {string}
 */
+export var OrganizationTypeEnum;
+(function (OrganizationTypeEnum) {
+    OrganizationTypeEnum["Community"] = "community";
+    OrganizationTypeEnum["University"] = "university";
+    OrganizationTypeEnum["Company"] = "company";
+    OrganizationTypeEnum["HighSchool"] = "high_school";
+})(OrganizationTypeEnum || (OrganizationTypeEnum = {}));
+/**
+* @export
+* @enum {string}
+*/
 export var RankedUserClassDecorationEnum;
 (function (RankedUserClassDecorationEnum) {
     RankedUserClassDecorationEnum["None"] = "none";
     RankedUserClassDecorationEnum["Silver"] = "silver";
     RankedUserClassDecorationEnum["Gold"] = "gold";
 })(RankedUserClassDecorationEnum || (RankedUserClassDecorationEnum = {}));
-/**
-* @export
-* @enum {string}
-*/
-export var SchemaClassDecorationEnum;
-(function (SchemaClassDecorationEnum) {
-    SchemaClassDecorationEnum["None"] = "none";
-    SchemaClassDecorationEnum["Silver"] = "silver";
-    SchemaClassDecorationEnum["Gold"] = "gold";
-})(SchemaClassDecorationEnum || (SchemaClassDecorationEnum = {}));
-/**
-* @export
-* @enum {string}
-*/
-export var Schema1ScreenThemeEnum;
-(function (Schema1ScreenThemeEnum) {
-    Schema1ScreenThemeEnum["Default"] = "default";
-    Schema1ScreenThemeEnum["Light"] = "light";
-    Schema1ScreenThemeEnum["Dark"] = "dark";
-    Schema1ScreenThemeEnum["Black"] = "black";
-})(Schema1ScreenThemeEnum || (Schema1ScreenThemeEnum = {})); /**
-* @export
-* @enum {string}
-*/
-export var Schema1ProblemSortByEnum;
-(function (Schema1ProblemSortByEnum) {
-    Schema1ProblemSortByEnum["Id"] = "id";
-    Schema1ProblemSortByEnum["Level"] = "level";
-    Schema1ProblemSortByEnum["Title"] = "title";
-    Schema1ProblemSortByEnum["Solved"] = "solved";
-    Schema1ProblemSortByEnum["AverageTry"] = "average_try";
-    Schema1ProblemSortByEnum["Random"] = "random";
-})(Schema1ProblemSortByEnum || (Schema1ProblemSortByEnum = {}));
-/**
- * 언어입니다.
- * @export
- * @enum {string}
- */
-export var Schema10;
-(function (Schema10) {
-    Schema10["Ko"] = "ko";
-    Schema10["En"] = "en";
-})(Schema10 || (Schema10 = {}));
-/**
-* @export
-* @enum {string}
-*/
-export var Schema12TypeEnum;
-(function (Schema12TypeEnum) {
-    Schema12TypeEnum["Community"] = "community";
-    Schema12TypeEnum["University"] = "university";
-    Schema12TypeEnum["Company"] = "company";
-    Schema12TypeEnum["HighSchool"] = "high_school";
-})(Schema12TypeEnum || (Schema12TypeEnum = {}));
-/**
-* @export
-* @enum {string}
-*/
-export var Schema6ClassDecorationEnum;
-(function (Schema6ClassDecorationEnum) {
-    Schema6ClassDecorationEnum["None"] = "none";
-    Schema6ClassDecorationEnum["Silver"] = "silver";
-    Schema6ClassDecorationEnum["Gold"] = "gold";
-})(Schema6ClassDecorationEnum || (Schema6ClassDecorationEnum = {}));
-/**
- * 난이도 아이콘 표시 정책입니다.
- * @export
- * @enum {string}
- */
-export var Schema9;
-(function (Schema9) {
-    Schema9["Default"] = "default";
-    Schema9["HideWarn"] = "hide-warn";
-    Schema9["Hide"] = "hide";
-})(Schema9 || (Schema9 = {}));
 /**
 * @export
 * @enum {string}
