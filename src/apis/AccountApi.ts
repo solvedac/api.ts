eef770ee648dcb1cb6224c31ec331d5994e32165
RanolP <RanolP@users.noreply.github.com>
2021-11-28 10:31:08 +0000

    Follow-up solvedac/unofficial-documentation

--- src/apis/AccountApi.ts
+++ src/apis/AccountApi.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 /**
  * @solvedac/unofficial-documentation
- * 이 프로젝트는 [solved.ac](https://solved.ac/) API를 문서화하는 커뮤니티 프로젝트입니다. 이 저장소는 원작자의 요청에 따라 언제든 지워질 수 있으며, 현재 API와 일치하지 않을 수도 있는 점 양해 부탁드립니다.  <sup>   solved.ac 서비스는 shiftpsh가 기획·개발·디자인·운영하는 프로젝트로,   이 저장소와는 solved.ac의 API를 문서화해둔 것 이외에는 아무런 관련이 없습니다. </sup>  [GitHub에서 보기](https://github.com/solvedac/unofficial-documentation)  ![@solvedac/unofficial-documentation banner](./assets/solvedac-ud-compact.png) 
+ * 이 프로젝트는 [solved.ac](https://solved.ac/) API를 문서화하는 커뮤니티 프로젝트입니다. 이 저장소는 원작자의 요청에 따라 언제든 지워질 수 있으며, 현재 API와 일치하지 않을 수도 있는 점 양해 부탁드립니다.  <sup>   solved.ac 서비스는 shiftpsh가 기획·개발·디자인·운영하는 프로젝트로,   이 저장소와는 solved.ac의 API를 문서화해둔 것 이외에는 아무런 관련이 없습니다. </sup>  **주의**: account 하위 루트를 탐색할 경우, 현재 로그인된 solvedacToken이 노출·오용될 수 있으니 주의하십시오.  [GitHub에서 보기](https://github.com/solvedac/unofficial-documentation)  ![@solvedac/unofficial-documentation banner](./assets/solvedac-ud-compact.png) 
  *
  * The version of the OpenAPI document: 3.2021.09+b2
  * Contact: public.ranolp@gmail.com
@@ -34,7 +34,7 @@ export interface UpdateAccountSettingsRequest {
 export class AccountApi extends runtime.BaseAPI {
 
     /**
-     * 리딤 코드와 배지, 배경 등을 교환합니다.
+     * 리딤 코드와 배지, 배경 등을 교환합니다.  **주의**: 테스트할 경우 현재 solved.ac에 로그인한 계정으로 요청을 전송합니다. 
      * 코드 교환하기
      */
     async redeemCodeRaw(requestParameters: RedeemCodeRequest, initOverrides?: RequestInit): Promise<runtime.ApiResponse<void>> {
@@ -60,7 +60,7 @@ export class AccountApi extends runtime.BaseAPI {
     }
 
     /**
-     * 리딤 코드와 배지, 배경 등을 교환합니다.
+     * 리딤 코드와 배지, 배경 등을 교환합니다.  **주의**: 테스트할 경우 현재 solved.ac에 로그인한 계정으로 요청을 전송합니다. 
      * 코드 교환하기
      */
     async redeemCode(requestParameters: RedeemCodeRequest, initOverrides?: RequestInit): Promise<void> {
@@ -68,7 +68,7 @@ export class AccountApi extends runtime.BaseAPI {
     }
 
     /**
-     * 계정의 설정을 변경합니다.
+     * 계정의 설정을 변경합니다.  **주의**: 테스트할 경우 현재 solved.ac에 로그인한 계정으로 요청을 전송합니다. 
      * 계정 설정 변경하기
      */
     async updateAccountSettingsRaw(requestParameters: UpdateAccountSettingsRequest, initOverrides?: RequestInit): Promise<runtime.ApiResponse<void>> {
@@ -94,7 +94,7 @@ export class AccountApi extends runtime.BaseAPI {
     }
 
     /**
-     * 계정의 설정을 변경합니다.
+     * 계정의 설정을 변경합니다.  **주의**: 테스트할 경우 현재 solved.ac에 로그인한 계정으로 요청을 전송합니다. 
      * 계정 설정 변경하기
      */
     async updateAccountSettings(requestParameters: UpdateAccountSettingsRequest, initOverrides?: RequestInit): Promise<void> {
@@ -102,7 +102,7 @@ export class AccountApi extends runtime.BaseAPI {
     }
 
     /**
-     * 현재 로그인한 계정 정보를 가져옵니다.
+     * 현재 로그인한 계정 정보를 가져옵니다.  **주의**: 테스트할 경우 현재 solved.ac에 로그인한 계정으로 요청을 전송합니다. 
      * 로그인 정보 가져오기
      */
     async verifyAccountCredentialsRaw(initOverrides?: RequestInit): Promise<runtime.ApiResponse<InlineResponse200>> {
@@ -121,7 +121,7 @@ export class AccountApi extends runtime.BaseAPI {
     }
 
     /**
-     * 현재 로그인한 계정 정보를 가져옵니다.
+     * 현재 로그인한 계정 정보를 가져옵니다.  **주의**: 테스트할 경우 현재 solved.ac에 로그인한 계정으로 요청을 전송합니다. 
      * 로그인 정보 가져오기
      */
     async verifyAccountCredentials(initOverrides?: RequestInit): Promise<InlineResponse200> {
